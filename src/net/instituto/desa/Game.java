e5fbf812b078b0bd1ec4f85bb0d82c33e423aaa4
Juan <juananguillo@gmail.com>
2020-03-03 19:59:47 +0100

    Cambios

--- src/net/instituto/desa/Game.java
+++ src/net/instituto/desa/Game.java
@@ -1,20 +1,21 @@
 package net.instituto.desa;
 
 public class Game {
-	Jugador j;
-
-	public void movimiento(String m) {
-		if (m.equalsIgnoreCase("Derecha")) {
-			j.setX(j.getX() + 1);
-		}
-		if (m.equalsIgnoreCase("Izquierda")) {
-			j.setX(j.getX() - 1);
-		}
-		if (m.equalsIgnoreCase("Arriba")) {
-			j.setY(j.getY() - 1);
-		}
-		if (m.equalsIgnoreCase("Abajo")) {
-			j.setY(j.getY() + 1);
-		}
+	static final String Abajo = "Abajo";
+	static final String Arriba = "Arriba";
+	static final String Izquierda = "Izquierda";
+	static final String Derecha = "Derecha";
+	
+	
+	public static void main(String[] args) {
+		Jugador j=new Jugador(1,2);
+		j.movimiento(Izquierda);
+		System.out.print(j.toString());
 	}
-}
+	}
+	
+	
+
+
+
+
--- src/net/instituto/desa/Jugador.java
+++ src/net/instituto/desa/Jugador.java
@@ -2,6 +2,15 @@ package net.instituto.desa;
 
 public class Jugador {
 	int y, x;
+	
+	
+	
+	
+
+	public Jugador(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
 	public int getY() {
 		return y;
@@ -19,4 +28,28 @@ public class Jugador {
 		this.x = x;
 	}
 
+	public void movimiento(String m) {
+		if (m.equalsIgnoreCase(Game.Derecha)) {
+			setX(getX() + 1);
+		}
+		if (m.equalsIgnoreCase(Game.Izquierda)) {
+			setX(getX() - 1);
+		}
+		if (m.equalsIgnoreCase(Game.Arriba)) {
+			setY(getY() - 1);
+		}
+		if (m.equalsIgnoreCase(Game.Abajo)) {
+			setY(getY() + 1);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [y=" + y + ", x=" + x + "]";
+	}
+
+	
+	
+	
+	
 }
